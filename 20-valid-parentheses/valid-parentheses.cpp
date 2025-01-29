dfb4df6bf69cec9bb86bab93144f55e205cb2650
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-01-29 23:50:12 +0530

    Time: 3 ms (7.43%) | Memory: 8.8 MB (33.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,34 +2,24 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                st.push(s[i]);
-            } else {
-                if (st.empty()) {
+        
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                if(st.empty()) return false; // Prevents stack underflow
+                
+                if((c == ')' && st.top() != '(') ||
+                   (c == '}' && st.top() != '{') ||
+                   (c == ']' && st.top() != '[')) {
                     return false;
                 }
-                if (s[i] == '}') {
-                    if (st.top() == '{') {
-                        st.pop();
-                    } else {
-                        return false;
-                    }
-                } else if (s[i] == ']') {
-                    if (st.top() == '[') {
-                        st.pop();
-                    } else {
-                        return false;
-                    }
-                } else if (s[i] == ')') {
-                    if (st.top() == '(') {
-                        st.pop();
-                    } else {
-                        return false;
-                    }
-                }
+                
+                st.pop();
             }
         }
-        return st.empty();
+        
+        return st.empty(); // Stack should be empty if valid
     }
 };
