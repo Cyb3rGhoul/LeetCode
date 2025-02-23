25c0508d56fea87909e52f3bd521c86544f5a8f2
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-02-23 20:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.21%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -1,22 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    void inOrder(TreeNode* root, string& s){
+        if(!root) return;
+        if(root->left == NULL && root->right==NULL){
+            s = s + to_string(root->val) + '_';
+        }
+        inOrder(root->left,s);
+        inOrder(root->right,s);
+    }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        function<void(TreeNode*, vector<int>&)> collectLeafValues =
-            [&](TreeNode* root, vector<int>& leafValues) {
-                if (!root) {
-                    return;
-                }
-                if (!root->left && !root->right) {
-                    leafValues.push_back(root->val);
-                }
-                collectLeafValues(root->left, leafValues);
-                collectLeafValues(root->right, leafValues);
-            };
+        string s1 = "";
+        string s2 = "";
 
-        vector<int> leafValues1, leafValues2;
-        collectLeafValues(root1, leafValues1);
-        collectLeafValues(root2, leafValues2);
+        inOrder(root1, s1);
+        inOrder(root2, s2);
 
-        return leafValues1 == leafValues2;
+        return s1==s2;
     }
 };
