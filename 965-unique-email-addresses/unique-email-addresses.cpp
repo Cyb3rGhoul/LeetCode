b8de75755ea3ad47f0f04d7f0ae41194eb7a8de8
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-02-24 23:23:22 +0530

    Time: 1 ms (93.50%) | Memory: 16.5 MB (94.49%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.cpp
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         int n = emails.size();
-        bool at = false;
+        
         int ctn = 0;
-        unordered_map<string,int> mp;
-        string s1 = "", s2 = "";
+        unordered_set<string> st;
+        string s1 = "";
         for(int i = 0; i<n; i++){
+            bool at = false;
             for(int k = 0; k<emails[i].length(); k++){
                 if(emails[i][k]=='.' && !at) continue;
                 else if(emails[i][k]=='+' && !at){
@@ -23,35 +24,9 @@ public:
                     s1 += emails[i][k];
                 }
             }
-            cout<<"s1: "<<s1<<endl;
-            at = false;
-            for(int j = i; j<n; j++){
-                if(j==i) continue;
-                for(int k = 0; k<emails[j].length(); k++){
-                    if(emails[j][k]=='.' && !at) continue;
-                    else if(emails[j][k]=='+' && !at){
-                        while(emails[j][k]!='@'){
-                            k++;
-                        }
-                        s2+='@';
-                        at = true;
-                    }
-                    else if(emails[j][k]=='@'){
-                        s2+='@';
-                        at = true;
-                    }
-                    else{
-                        s2 += emails[j][k];
-                    }
-                }
-                at = false;
-                cout<<"s2: "<<s2<<endl;
-                mp[s2]++;
-                s2.erase();
-            }
-            mp[s1]++;
+            st.insert(s1);
             s1.erase();
         }
-        return mp.size();
+        return st.size();
     }
 };
