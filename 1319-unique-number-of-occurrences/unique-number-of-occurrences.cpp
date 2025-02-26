1712ae3a8553ff5ff9ad6690fac5d1d2d2215ce3
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-02-26 21:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (27.67%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> mp;
         int n = arr.size();
+        unordered_map<int,int> mp;
         for(int i = 0; i<n; i++){
             mp[arr[i]]++;
         }
-
         unordered_set<int> st;
-        for(auto it: mp){
+        for(auto& it: mp){
             st.insert(it.second);
         }
-
-        return st.size() == mp.size();
+        return st.size()==mp.size();
     }
 };
