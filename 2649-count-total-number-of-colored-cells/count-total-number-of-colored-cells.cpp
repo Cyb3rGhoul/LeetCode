e93ab6fb8c54295952392ee715767c151bedf5ec
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-03-05 21:47:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (31.17%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long res = 1;
+        for(int i = 1; i<n; i++){
+            res += (4*i);
+        }
+        return res;
+    }
+};
