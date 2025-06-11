9b1298d0a4724bee957b81689ef2edfa1c2fe8f2
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-06-11 15:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.76%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,6 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int idx = -1;
+
+        //piche se sabse pehele pahad ka dip dhundho
         for(int i = n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 idx = i;
@@ -10,11 +12,13 @@ public:
             }
         }
 
+        //agar idx -1 raha matalb ki wo lexiographically sabse bada hai
         if(idx==-1) {
             reverse(nums.begin(),nums.end());
             return;
         }
 
+        //aab piche se sabse pehele index nikalo jo ki nums[idx] se bada ho
         for(int i = n-1; i>idx; i--){
             if(nums[i]>nums[idx]){
                 swap(nums[i],nums[idx]);
@@ -22,6 +26,8 @@ public:
             }
         }
 
+
+        //aab bas reverse kardo
         reverse(nums.begin()+idx+1,nums.end());
 
     }
