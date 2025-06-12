850b8b07b0b8504522e5e18a494b12445fe2c442
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-06-12 14:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (69.32%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -7,16 +7,22 @@ public:
         slow = nums[slow];
         fast = nums[nums[fast]];
 
+
+        // THIS LOOP IS TO FIND THE LOOP AND IT IS BASICALLY THE FIRST STEP IN GETTING THE LOOP STARTING POINT
         while(slow!=fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
 
+
+        //WE AGAIN PUT THE SLOW TO START AND AS THE SLOW WILL REACH THE LOOP STARTING POINT MAGICALLY THE FAST WILL ALSO REACH THE LOOP STARTING POINT
         slow = nums[0];
         while(slow!=fast){
             slow = nums[slow];
             fast = nums[fast];
         }
+
+        //RETURN FAST
         return fast;
     }
 };
