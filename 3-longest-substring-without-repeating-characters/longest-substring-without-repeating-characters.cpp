210856636cf04459be0c749d4fcb428331794abd
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-06-13 22:58:41 +0530

    Time: 10 ms (54.45%) | Memory: 11.8 MB (58.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //ek approach soch ra hu ki map mai dal dunga jesse hi repeat ho to ek minus kar  dunga
+        //agar zero ho jayega to element nikal dunga and size map ka size rakh lunga
+
+        //nahi ye work nahi karegi
+
+        //two pointer sliding window
+
         int n = s.length();
-        int maxLength = 0;
-        vector<int> charIndex(128, -1);
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (charIndex[s[right]] >= left) {
-                left = charIndex[s[right]] + 1;
+        unordered_map<char,int> freq;
+
+        if(n==0) return 0;
+
+        int l=0;
+        int r=0;         
+        int maxi = 1;
+        while(r<n){
+            freq[s[r]]++;
+            if(freq[s[r]]==2){
+                while(s[l]!=s[r]){
+                    freq[s[l++]]--;
+                }
+                freq[s[l++]]--;
             }
-            charIndex[s[right]] = right;
-            maxLength = max(maxLength, right - left + 1);
+            r++;
+            maxi = max(r-l,maxi);
         }
-        
-        return maxLength;
+        return maxi;
     }
 };
