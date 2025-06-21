66a3731c96813ea6bb0c58eaf1a9cd6aa15dae79
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-06-21 11:55:39 +0530

    Time: 11 ms (23.13%) | Memory: 19.9 MB (48.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,61 +1,40 @@
 class Solution {
 public:
+    bool dfs(unordered_map<int,vector<int>>& adj, int u, vector<bool>& vis, vector<bool>& inRec){
 
-    bool topo(unordered_map<int,vector<int>> adj, int n, vector<int>& indegree){
-        queue<int> q;
+        vis[u] = true;
+        inRec[u] = true;
 
-        //kitane node maine visit kar liye
-        int ctn = 0;
-
-        for(int i = 0; i<n; i++){
-            if(indegree[i]==0) {
-                q.push(i);
-                ctn++;
+        for(int &v: adj[u]){
+            if(!vis[v] && dfs(adj,v,vis,inRec)){
+                return true; //cycle to hai
             }
+            else if(inRec[v]==true) return true;
         }
+        inRec[u] = false;
+        return false;
 
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-
-            for(int &v: adj[u]){
-                indegree[v]--;
-                if(indegree[v]==0) {
-                    q.push(v);
-                    ctn++;
-                }
-            }
-        }
-
-        return (ctn==n); //I was able to visit all nodes(course)
-        //i.e I was able to finish all courses
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        //dekho isme humko bantana hai ki hum sare course kar payenge ki nahi
+        vector<bool> vis(numCourses,false);
+        vector<bool> inRec(numCourses,false);
 
-        // the only constraint in doing all the course ki a--->b and b--->a matalb
-        // cycle form ho ri hogi
-
-        //topological sort dekhte hai.
-
-        //graph banao
         unordered_map<int,vector<int>> adj;
-        vector<int> indegree(numCourses,0);
 
         for(auto &vec: prerequisites){
             int a = vec[0];
             int b = vec[1];
 
-            //b-->a;
-
             adj[b].push_back(a);
-
-            //arrow jaa raha hai 'a' me
-            indegree[a]++;
         }
 
-        return topo(adj,numCourses,indegree);
+        for(int i = 0; i<numCourses; i++){
+            if(!vis[i] && dfs(adj,i,vis,inRec)){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
