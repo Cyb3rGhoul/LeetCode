6df86e1234bb4b9b88c9f121bb69b1e01dbf8ef8
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-07-05 20:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 74.4 MB (37.14%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -3,26 +3,17 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n = shifts.size();
         vector<int> pref(n);
-        pref[n-1] = (shifts[n-1])%26;
+        pref[n-1] = shifts[n-1];
 
         for(int i = n-2; i>=0; i--){
-            int val = (pref[i+1])%26;
+            int val = (pref[i+1]);
             pref[i] = (shifts[i] + val)%26;
         }
         
-        string res = "";
         for(int i = 0; i<n; i++){
-            int num = s[i] + pref[i];
-            while(num>122) {
-                num -= 122; 
-            }
-            if(num<96){
-                res += 'a' + num - 1;
-                continue;
-            }
-            res += s[i] + pref[i];
+            s[i] = ('a'+(s[i]-'a'+pref[i])%26);
         }
-        return res;
+        return s;
 
     }
 };
