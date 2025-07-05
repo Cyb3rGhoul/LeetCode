1fdeb7a247d5ccbdf5fd4d48d601a7041bcc0184
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-07-05 22:23:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (86.96%) - LeetSync

---
+++ 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestString(string s) {
+        int n = s.length();
+
+        int i = 0;
+        while(i<n && s[i]=='a'){
+            i++;
+        }
+
+        if(i==n){
+            s[n-1] = 'z';
+            return s;
+        }
+
+        while(i<n && s[i]!='a'){
+            s[i]--;
+            i++;
+        }
+        
+        return s;
+    }
+};
