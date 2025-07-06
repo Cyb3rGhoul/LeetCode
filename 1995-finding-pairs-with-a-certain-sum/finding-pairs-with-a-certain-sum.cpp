38f16a98a3a702d85f4a864f835d5957b727a8f4
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-07-06 17:12:25 +0530

    Time: 79 ms (78.71%) | Memory: 82 MB (70.15%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,53 +1,32 @@
 class FindSumPairs {
-private:
-    unordered_map<int, int> nums1_freq;  // Frequency map for nums1
-    vector<int> nums2;                   // Copy of nums2 for modifications
-    unordered_map<int, int> nums2_freq;  // Frequency map for current nums2 state
-    
 public:
-    FindSumPairs(vector<int>& nums1, vector<int>& nums2_input) {
-        // Build frequency map for nums1
-        nums1_freq.reserve(nums1.size());
-        for(int num : nums1) {
-            nums1_freq[num]++;
-        }
-        
-        // Copy nums2 and build its frequency map
-        nums2 = nums2_input;
-        nums2_freq.reserve(nums2.size());
-        for(int num : nums2) {
-            nums2_freq[num]++;
+    vector<int> arr1, arr2;
+    unordered_map<int, int> freq;
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        // khoi tao va dem freq trong arr2
+        arr1 = nums1;
+        arr2 = nums2;
+        for(auto& a2 : arr2){
+            freq[a2]++;
         }
     }
     
     void add(int index, int val) {
-        // Update frequency map: remove old value, add new value
-        int old_val = nums2[index];
-        int new_val = old_val + val;
-        
-        // Update frequency map
-        nums2_freq[old_val]--;
-        if(nums2_freq[old_val] == 0) {
-            nums2_freq.erase(old_val);
-        }
-        nums2_freq[new_val]++;
-        
-        // Update the actual array
-        nums2[index] = new_val;
+        // giam, tang freq cua arr2[index] cu va moi
+        freq[arr2[index]]--;
+        arr2[index] += val;
+        freq[arr2[index]]++;
     }
     
     int count(int tot) {
-        int result = 0;
-        
-        // Iterate through nums2 frequency map instead of the array
-        for(const auto& [num2, freq2] : nums2_freq) {
-            int complement = tot - num2;
-            auto it = nums1_freq.find(complement);
-            if(it != nums1_freq.end()) {
-                result += it->second * freq2;
+        int ans = 0;
+        for(auto& a1 : arr1){
+            // voi moi a1, map no voi tat ca a2 -> cong vao ans
+            int a2 = tot - a1;
+            if(freq.count(a2)){
+                ans += freq[a2];
             }
         }
-        
-        return result;
+        return ans;
     }
 };
