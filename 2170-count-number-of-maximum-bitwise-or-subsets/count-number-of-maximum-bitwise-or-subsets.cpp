8161c9e3fa6bf9ea472634779426ccb0fd343669
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-07-28 09:17:55 +0530

    Time: 11 ms (65.94%) | Memory: 10.7 MB (43.96%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int idx, vector<int> &temp, unordered_map<int,vector<vector<int>>> &mp, int sum){
-        if(idx == nums.size()){
-            mp[sum].push_back(temp);
+    int maxOr = 0;
+    int count = 0;
+    
+    void solve(vector<int>& nums, int idx, int currentOr) {
+        if(idx == nums.size()) {
+            if(currentOr > maxOr) {
+                maxOr = currentOr;
+                count = 1;
+            } else if(currentOr == maxOr) {
+                count++;
+            }
             return;
         }
         
         // Include current element
-        temp.push_back(nums[idx]);
-        solve(nums, idx+1, temp, mp, sum | nums[idx]);
-        temp.pop_back();
+        solve(nums, idx + 1, currentOr | nums[idx]);
         
-        // Exclude current element  
-        solve(nums, idx+1, temp, mp, sum);
+        // Exclude current element
+        solve(nums, idx + 1, currentOr);
     }
     
     int countMaxOrSubsets(vector<int>& nums) {
-        unordered_map<int,vector<vector<int>>> mp;
-        vector<int> temp;
-        solve(nums, 0, temp, mp, 0);
-        
-        int ans = 0;
-        int maxi = INT_MIN;
-        for(auto &it: mp){
-            cout << it.first << " " << it.second.size() << endl;
-            if(maxi < it.first){
-                maxi = it.first;
-                ans = it.second.size(); 
-            }
-        }
-        return ans;
+        solve(nums, 0, 0);
+        return count;
     }
 };
