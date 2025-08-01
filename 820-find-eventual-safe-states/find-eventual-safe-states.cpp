f1cc1059f8915aa596d6f964be0621a91ab4a518
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-08-01 22:51:57 +0530

    Time: 2 ms (96.21%) | Memory: 52.3 MB (84.59%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
+private:
+    bool dfs(int node, vector<vector<int>>& adj, vector<int> &vis, vector<int> &pathVis) {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                if(dfs(it, adj, vis, pathVis) == true) return true;
             }
+            else if(pathVis[it]) return true;
         }
+
+        pathVis[node] = 0;
+        return false;
     }
-    
+
+public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
-        queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+        int V = graph.size();
+        
+        // No need to create a separate adjacency list since graph is already in that format
+        vector<int> vis(V,0);
+        vector<int> pathVis(V,0);
+        vector<int> safe;
+
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, graph, vis, pathVis);
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        
+        // A node is safe if it's not part of any cycle
+        for(int i = 0; i < V; i++) {
+            if(vis[i] && !pathVis[i]) {
+                safe.push_back(i);
+            }
+        }
+        
+        return safe;
     }
 };
