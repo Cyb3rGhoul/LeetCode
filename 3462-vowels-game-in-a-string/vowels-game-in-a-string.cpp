957487f329d7875fa806eca09f4036a9581d4cfe
HARSH SHUKLA <116831986+Cyb3rGhoul@users.noreply.github.com>
2025-09-12 21:56:02 +0530

    Time: 35 ms (5.04%) | Memory: 28.5 MB (5.41%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int n = s.length();
+        vector<int> freq(n,0);
+        string vow = "aeiou";
+        if(vow.find(s[0])!=string::npos) freq[0] = 1;
+
+        for(int i = 1; i<n; i++){
+            if(vow.find(s[i])!=string::npos) freq[i] = freq[i-1] + 1;
+            else freq[i] = freq[i-1];
+        }
+
+        // for(int i : freq){
+        //     cout<<i<<endl;
+        // }
+        if(freq[n-1]==0) return false;
+        if(freq[n-1]==1||freq[n-1]%3==0||(freq[n-1]-1)%3==0||(freq[n-1]+1)%3==0) return true;
+        return false;
+        
+    }
+};
